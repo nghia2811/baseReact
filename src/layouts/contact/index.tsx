3bff17c05f1a647a8775f0178f8c2aeacd985b7d
NTNghia <ntnghia.hust@gmai.com>
2022-03-17 16:57:06 +0700

    add bootstrap

--- src/layouts/contact/index.tsx
+++ src/layouts/contact/index.tsx
@@ -8,6 +8,11 @@ function Contact() {
         The easiest thing to do is post on our{" "}
         <a href="http://forum.kirupa.com">forums</a>.
       </p>
+      <div className="d-flex flex-row border border-primary rounded">
+        <h2>NTNGHIA</h2>
+        <h2>28/11/1999</h2>
+        <h2>HUST</h2>
+      </div>
     </div>
   );
 }
